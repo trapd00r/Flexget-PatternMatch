a37be16fbe970ca932a8f6313035aea7dd9383ec
Magnus Woldrich <trapd00r@trapd00r.se>
2010-09-20 11:58:11 +0200

    Flexget::PatternMatch: Complete rewrite. Added support for ANSI escape sequences and dzen notation. Returns a hashref now

--- PatternMatch.pm
+++ PatternMatch.pm
@@ -4,52 +4,115 @@ require Exporter;
 @EXPORT_OK = qw(patternmatch);
 
 use Data::Dumper;
+use strict;
 
 our %patterns = (
-  'S[0-9]{2}E[0-9]{2}'                  => "\033[38;5;198mNew Episode",
-  'S01E01'                              => "\033[38;5;196mNew Show",
+  'S[0-9]{2}E[0-9]{2}'               => {
+    ansi  => "\e[38;5;198mNew Episode\e[0m",
+    dzen  => "^fg(#ff0000)New Episode^fg()",
+  },
+  'S01E01'                           => {
+    ansi  => "\e[38;5;196mNew Show\e[0m",
+    dzen  => "^fg(#ffff00)New Show^fg()",
+  },
   'do(c|k?)u(ment.+)?|
-    (discovery|history)\.(channel)?|
-    national\.geographic|
-    colossal\..+'                       => "\033[38;5;112mDocumentary",
-  'EPL|WWE|UFC|UEFA|Rugby|La\.Liga|
-    Superleague|Allsvenskan|
-    Formula\.(Ford|[0-9]{4})'           => "\033[38;5;144mSport",
-  '(?i)swedish|-se-'                    => "\033[38;5;122mSwedish",
-  '(?i)jay\.leno'                       => "\033[38;5;111mTalk Show",
-
-  'PsyCZ|MYCEL|UPE|HiEM|PSi|gEm'        => "\033[38;5;192m\033[1mPsychedelic",
+  (discovery|history)\.(channel)?|
+  national\.geographic|
+  colossal\.'                        => {
+    ansi  =>  "\e[38;5;112Documentary\e[0m",
+    dzen  =>  "^fg(#87d700)Documentary^fg()",
+  },
+  'EPL|WWE|UFC|UEFA|Rugby|La\Liga|
+  Superleague|Allsvenskan|
+  Formula\.Ford'                     => {
+    ansi  =>  "\e[38;5;144mSport\e[0m",
+    dzen  =>  "^fg(#afaf87)Sport^fg()",
+  },
+  '(?i)swedish|-se-'                 => {
+    ansi  =>  "\e[38;5;122mSwedish\e[0m",
+    dzen  =>  "^fg(#87ffd7)Swedish^fg()",
+  },
+  '(?i)jay\.leno'                    => {
+    ansi  =>  "\e[38;5;111Talk Show\e[0m",
+    dzen  =>  "^fg(87afff)Talk Show^fg()",
+  },
+  'PsyCZ|MYCEL|UPE|HiEM|PSi|gEm'     => {
+    ansi  =>  "\e[38;5;192mPsychedelic\e[0m",
+    dzen  =>  "^fg(#d7ff87)Psychedelic^fg()",
+  },
   '.+-(H3X|wAx|CMS|BFHMP3|WHOA|RNS|
-    C4|CR|UMT|0MNi(.+)?|FRAY(.+)?)$'    => "\033[38;5;94m\033[1mHip-Hop",
-  'LzY|qF|SRP|NiF'                      => "\033[38;5;126mRock/Metal",
-  'TALiON|HB|DV8'                       => "\033[38;5;41m\033[1mHardstyle",
-  '-sour$'                              => "\033[38;5;166mDnB",
-  'VA(-|_-_).+'                         => "\033[38;5;49mVarious",
-  '\(?_?-?WEB-?_?\)?'                   => "\033[38;5;190mWEB",
-  '\(?_?-?CDS-?_?\)?'                   => "\033[38;5;244mCDS",
-  '\(?_?-?CDM-?_?\)?'                   => "\033[38;5;233mCDM",
-  '\(?_?-?CDA-?_?\)?'                   => "\033[38;5;222mCDA",
-  '\(?_?-?DAB-?_?\)?'                   => "\033[38;5;211mDAB",
-  '\(?_?-?VLS|Vinyl-?_?\)?'             => "\033[38;5;201m\033[1mVLS",
-  '\(?_?-?CABLE-?_?\)?'                 => "\033[38;5;191mCABLE",
-  'Live_(on|at|in)'                     => "\033[38;5;181mLIVE",
-  '-Recycled.+$'                        => "\033[38;5;215mRe-release",
+  C4|CR|UMT|0MNi)(.+)?|FRAY(.+)?$'   => {
+    ansi  =>  "\e[38;5;094mHip-Hop\e[0m",
+    dzen  =>  "^fg(#875f00)Hip-Hop^fg()",
+  },
+  'LzY|qF|SRP|NiF'                   => {
+    ansi  =>  "\e[38;5;126mRock\e[0m",
+    dzen  =>  "^fg(#af0087)Rock^fg()",
+  },
+  '-sour$'                           => {
+    ansi  =>  "\e[38;5;166mDnB\e[0m",
+    dzen  =>  "^fg(#d75f00)DnB^fg()",
+  },
+  'VA(-|_-_).+'                      => {
+    ansi  =>  "\e[38;5;049mV/A\e[0m",
+    dzen  =>  "^fg(#00ffaf)V/A^fg()",
+  },
+  '\(?_?-?CDS-?_?\)?'                => {
+    ansi  =>  "\e[38;5;244mSingle\e[0m",
+    dzen  =>  "^fg(#808080)Single^fg()",
+  },
+  '\(?_?-?CDM-?_?\)?'                => {
+    ansi  => "\e[38;5;233mMaxi\e[0m",
+    dzen  => "^fg(#1c1c1c)Maxi^fg()",
+  },
+  '\(?_?-?CDA-?_?\)?'                => {
+    ansi  => "\e[38;5;222mAlbum\e[0m",
+    dzen  => "^fg(#ffd787)Album^fg()",
+  },
+  '\(?_?-?DAB-?_?\)?'                => {
+    ansi  => "\e[38;5;211mDAB\e[0m",
+    dzen  => "^fg(#ff87af)DAB^fg()",
+  },
+  '\(?_?-?CABLE-?_?\)?'              => {
+    ansi  => "\e[38;5;191mCable\e[0m",
+    dzen  => "^fg(#d7ff5f)Cable\e[0m",
+  },
+  '\(?_?-?VLS|Vinyl-?_?\)?'          => {
+    ansi  => "\e[38;5;201mVinyl\e[0m",
+    dzen  => "^fg(#ff00ff)Vinyl^fg()",
+  },
+  '\(?_?-?WEB-?_?\)?'                => {
+    ansi  => "\e[38;5;19mWEB\e[0m",
+    dzen  => "^fg(#ffabcd)WEB^fg()",
+  },
+  'Live_(on|at|in)'                  => {
+    ansi  => "\e[38;5;181mLive\e[0m",
+    dzen  => "^fg(#d7afaf)Live^fg()",
+  },
+  '-Recycled.+$'                     => {
+    ansi  => "\e[38;5;215Re-release\e[0m",
+    dzen  => "^fg(#ffaf5f)Re-release^fg()",
+  },
 
+  'TALiON|HB|DV8'       => {
+    ansi  => "\e[38;5;41m\e[1mHardstyle\e[0m",
+    dzen  => "^fg(#f95504)Hardstyle^fg()",
+  },
 );
 
 sub patternmatch {
-  my @files = @_;
-  my $nocolor = "\033[0m";
-  foreach my $file(@files) {
-    chomp($file);
-    $file = sprintf("%60s", $file);
-    $file = sprintf("%.60s", $file);
+  my $esc_style = shift;
+  chomp(my @files = @_);
+
+  my %results;
+  for my $file( @files ) {
     for my $pattern(keys(%patterns)) {
+
       if($file =~ m/$pattern/x) {
-        $file = $file. " \033[3m" .$patterns{$pattern}.$nocolor;
+        $results{$file} = $patterns{$pattern}{$esc_style};
       }
     }
   }
-  return @files;
+  return(\%results);
 }
 
